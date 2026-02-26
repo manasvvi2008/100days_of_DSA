a2ce9cef106d41e649e403840f2a7e43b2ac3459
Manasvvi <manasvvi52@gmail.com>
2026-02-26 20:00:37 +0530

    Day 22: DSA Practice

---
+++ 100days_of_DSA/Day22.c
@@ -0,0 +1,42 @@
+//Count Nodes in Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int count = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*) malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    temp = head;
+    while(temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
