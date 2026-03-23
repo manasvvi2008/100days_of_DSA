ef3dbbe4b93ab719e9f5e3817865bbe99b195df8
Manasvvi <manasvvi52@gmail.com>
2026-03-23 20:00:37 +0530

    Day 47: DSA Practice

---
+++ 100days_of_DSA/Day47.c
@@ -0,0 +1,108 @@
+//Height of Binary Tree
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* createNode(int data) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->data = data;
+    n->left = n->right = NULL;
+    return n;
+}
+
+typedef struct Queue {
+    Node** arr;
+    int front, rear, size, capacity;
+} Queue;
+
+Queue* createQueue(int capacity) {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->capacity = capacity;
+    q->front = q->size = 0;
+    q->rear = capacity - 1;
+    q->arr = (Node**)malloc(capacity * sizeof(Node*));
+    return q;
+}
+
+int isEmpty(Queue* q) {
+    return q->size == 0;
+}
+
+void enqueue(Queue* q, Node* item) {
+    q->rear = (q->rear + 1) % q->capacity;
+    q->arr[q->rear] = item;
+    q->size++;
+}
+
+Node* dequeue(Queue* q) {
+    Node* item = q->arr[q->front];
+    q->front = (q->front + 1) % q->capacity;
+    q->size--;
+    return item;
+}
+
+int height(Node* root) {
+    if (!root) return 0;
+    Queue* q = createQueue(1000);
+    enqueue(q, root);
+    int h = 0;
+    while (!isEmpty(q)) {
+        int nodeCount = q->size;
+        h++;
+        while (nodeCount--) {
+            Node* temp = dequeue(q);
+            if (temp->left) enqueue(q, temp->left);
+            if (temp->right) enqueue(q, temp->right);
+        }
+    }
+    return h;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n == 0) {
+        printf("0");
+        return 0;
+    }
+
+    int val;
+    scanf("%d", &val);
+    Node* root = createNode(val);
+
+    Queue* q = createQueue(1000);
+    enqueue(q, root);
+
+    int count = 1;
+
+    while (count < n) {
+        Node* curr = dequeue(q);
+
+        int l, r;
+
+        if (count < n) {
+            scanf("%d", &l);
+            count++;
+            if (l != -1) {
+                curr->left = createNode(l);
+                enqueue(q, curr->left);
+            }
+        }
+
+        if (count < n) {
+            scanf("%d", &r);
+            count++;
+            if (r != -1) {
+                curr->right = createNode(r);
+                enqueue(q, curr->right);
+            }
+        }
+    }
+
+    printf("%d", height(root));
+    return 0;
+}
